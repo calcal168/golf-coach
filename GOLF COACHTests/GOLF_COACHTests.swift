28ce99d319a5e6844edb32c1c670a73071231dde
Calvin Deng <calvindeng@hotmail.com>
2026-05-19 18:47:50 -0400

    Initial Commit

---
+++ GOLF COACHTests/GOLF_COACHTests.swift
@@ -0,0 +1,19 @@
+//
+//  GOLF_COACHTests.swift
+//  GOLF COACHTests
+//
+//  Created by Calvin Deng on 2026-05-19.
+//
+
+import Testing
+@testable import GOLF_COACH
+
+struct GOLF_COACHTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
